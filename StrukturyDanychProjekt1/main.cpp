928190fd4b932af84680f3c9193de35d3f1169c9
Zbynkus9 <kamil.pojedynek@gmail.com>
2025-04-14 21:58:13 +0200

    Dynamic Array Implementation

--- StrukturyDanychProjekt1/main.cpp
+++ StrukturyDanychProjekt1/main.cpp
@@ -136,6 +136,118 @@ private:
 	unsigned int size;
 };
 
+template<typename T>
+class DArrElement {
+public:
+	DArrElement(T val) : value(val) {}
+	getValue() const {
+		return value;
+	}
+private:
+	T value;
+};
+
+template<typename T>
+class DArray {
+public:
+	DArray(unsigned int cap = 10) : size(0), capacity(cap) {
+		arr = new DArrElement<T>[capacity];
+	}
+	~DArray() {
+		delete[] arr;
+	}
+	void resizeAndCopy() {
+		capacity *= 2;
+		DArrElement<T>* newArr = new DArrElement<T>[capacity];
+		for (unsigned int i = 0; i < size; i++) {
+			newArr[i] = arr[i];
+		}
+		delete[] arr;
+		arr = newArr;
+	};
+	insertOnEmpty(T val) {
+		arr[size++] = DArrElement<T>(val);
+	}
+	insertOnFront(T val) {
+		if (size == 0) insertOnEmpty(val);
+		else if (size == capacity) {
+			resizeAndCopy();
+			for (unsigned int i = size; i > 0; i--) {
+				arr[i] = arr[i - 1];
+			}
+			arr[0] = DArrElement<T>(val);
+			size++;
+		}
+		else {
+			for (unsigned int i = size; i > 0; i--) {
+				arr[i] = arr[i - 1];
+			}
+			arr[0] = DArrElement<T>(val);
+			size++;
+	}
+	}
+	void insertOnBack(T val) {
+		if (size == 0) insertOnEmpty(val);
+		else if (size == capacity) resizeAndCopy();
+		arr[size++] = DArrElement<T>(val);
+	}
+	void insertAt(T val, unsigned int index) {
+		if (index > size) return;
+		else if (index == 0) insertOnFront(val);
+		else if (index == size) insertOnBack(val);
+		else {
+			if (size == capacity) resizeAndCopy();
+			for (unsigned int i = size; i > index; i--) {
+				arr[i] = arr[i - 1];
+			}
+			arr[index] = DArrElement<T>(val);
+			size++;
+		}
+	}
+	void removeFront() {
+		if (size == 0) return;
+		for (unsigned int i = 0; i < size - 1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		size--;
+	}
+	void removeBack() {
+		if (size == 0) return;
+		delete arr[size - 1];
+		size--;
+	}
+	void removeAt(unsigned int index) {
+		if (index >= size) return;
+		else if (index == 0) removeFront();
+		else if (index == size - 1) removeBack();
+		else {
+			for (unsigned int i = index; i < size - 1; i++) {
+				arr[i] = arr[i + 1];
+			}
+			size--;
+		}
+	}
+	void clearArray() {
+		delete[] arr;
+		arr = new DArrElement<T>[capacity];
+		size = 0;
+	}
+	T findByIndex(unsigned int index) {
+		if (index >= size) throw out_of_range("Index out of range");
+		return arr[index].getValue();
+	}
+	T findByValue(T val) {
+		for (unsigned int i = 0; i < size; i++) {
+			if (arr[i].getValue() == val) return arr[i].getValue();
+		}
+		throw invalid_argument("Value not found");
+	}
+private:
+	DArrElement<T>* arr;
+	unsigned int size;
+	unsigned int capacity;
+};
+
 
 int main() {
 	cout << "Hello World!" << endl;
